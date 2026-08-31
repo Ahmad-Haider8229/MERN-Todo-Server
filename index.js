047cd07b9a8723ec702c10ae5911e3e9429d23c0
unknown <ahmadhaider4772@gmail.com>
2026-08-31 15:08:27 +0500

    tweaks

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express")
 const cors = require("cors")
-
+require('dotenv').config();
 const app = express()
 const {connectDB} = require("./config/db")
 const authRoutes = require("./routes/auth");
@@ -14,15 +14,15 @@ app.use("/todo", todos);
 app.use(express.urlencoded({ extended: true }));
 connectDB()
 
+const PORT = process.env.PORT || 8000;
+const MONGODB_URI = process.env.MONGODB_URI;
+
+console.log('Server running on port:', PORT);
+console.log('Environment:', process.env.NODE_ENV);
 
 app.get("/",(req,res) => {
 
 res.send("Server ")
 })
 
-app.listen(8000,  (req,res) => {
-
-console.log("Server is running ")
-
-})
 
