065dc3a29bcc07047c7210020978dd303b8e165b
unknown <ahmadhaider4772@gmail.com>
2026-09-03 21:43:59 +0500

    json

--- config/db.js
+++ config/db.js
@@ -1,20 +1,31 @@
 const mongoose = require('mongoose');
+const express = require("express")
+require('dotenv').config();
 const dns = require("dns")
 dns.setDefaultResultOrder('ipv4first');
 dns.setServers(["1.1.1.1", "8.8.8.8"])
-const connectDB = () => {
+const app = express()
+let isconnected = false
+const connectDB = async () => {
 
-mongoose.connect('mongodb+srv://ahmadhaider4772_db_user:dFBKRZ6uepFobZpx@cluster0.zyguhhf.mongodb.net/?appName=Cluster0').then(()=>{
+await mongoose.connect("mongodb+srv://ahmadhaider4772_db_user:dFBKRZ6uepFobZpx@cluster0.zyguhhf.mongodb.net/?appName=Cluster0").then(()=>{
 
 console.log("Connected")
-
+isconnected = true
 }).catch((error)=>{
 console.log(error)
 console.log("Not connected")
 });
 
 
-
 }
+
+
+app.use((req,res,next) => {
+    if(isconnected){
+        connectDB()
+    }
+    next()
+})
 
 module.exports = {connectDB}
